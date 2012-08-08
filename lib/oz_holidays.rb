f5b11764dc55d7431c0f283cdac012fbd1e8107d
Andrew Snow <andrew@modulus.org>
2012-08-08 19:38:58 +1000

    convert string dates to real ruby dates

--- lib/oz_holidays.rb
+++ lib/oz_holidays.rb
@@ -1,5 +1,5 @@
 
-require 'json' rescue nil
+require 'date'
 
 class OzHolidays
   def self.national
@@ -15,131 +15,134 @@ class OzHolidays
   def self.to_json
     by_state.to_json
   end
+  
+  def self.by_date
+    by(:date, :title)
+  end
 
+  def self.by(field1, field2)
+    field3 = (all.first.keys - [field1, field2]).first
+    result = Hash.new {|h,k| h[k] = {} }
+    all.inject(Hash.new {|h,k| h[k] = {} }) do |result, hol|
+      if result[hol[field1]].has_key? hol[field2]
+        result[hol[field1]][hol[field2]] = [result[hol[field1]][hol[field2]], hol[field3]].flatten
+      else
+        result[hol[field1]][hol[field2]] = hol[field3]
+      end
+      result
+    end
+  end
+  
   def self.by_state
-    {
-      'QLD' => {
-        '2011-04-25' => 'ANZAC Day',
-        '2011-04-26' => 'Easter Monday',
-        '2011-05-02' => 'Labour Day',
-        '2011-06-13' => 'Queens Birthday',
-        '2011-08-17' => 'Royal Qland Show metro',
-        '2012-05-07' => 'Labour Day',
-        '2012-06-11' => 'Queens Birthday',
-        '2012-08-15' => 'Royal Qland Show metro',
-        '2013-03-30' => 'Easter Saturday',
-        '2013-05-06' => 'Labour Day',
-        '2013-10-07' => 'Queens Birthday'
-      },
-      'SA' => {
-        '2011-03-14' => 'Adelaide Cup',
-        '2011-04-25' => 'Easter Monday',
-        '2011-04-26' => 'ANZAC Day',
-        '2011-06-13' => 'Queens Birthday',
-        '2011-10-03' => 'Labour Day',
-        '2012-03-12' => 'Adelaide Cup',
-        '2012-06-11' => 'Queens Birthday',
-        '2012-10-01' => 'Labour Day',
-        '2013-03-11' => 'Adelaide Cup',
-        '2013-03-30' => 'Easter Saturday',
-        '2013-06-10' => 'Queens Birthday',
-        '2013-10-07' => 'Labour Day'
-      },
-      'NSW' => {
-        '2011-04-25' => 'ANZAC Day',
-        '2011-04-26' => 'Easter Monday',
-        '2011-06-13' => 'Queens Birthday',
-        '2011-10-03' => 'Labour Day',
-        '2012-06-11' => 'Queens Birthday',
-        '2012-10-01' => 'Labour Day',
-        '2013-03-30' => 'Easter Saturday',
-        '2013-06-10' => 'Queens Birthday',
-        '2013-10-07' => 'Labour Day'
-      },
-      'WA' => {
-        '2011-03-07' => 'Labour Day',
-        '2011-04-25' => 'Easter Monday',
-        '2011-04-26' => 'ANZAC Day',
-        '2011-06-06' => 'Foundation Day',
-        '2011-10-03' => 'Queens Birthday',
-        '2012-03-05' => 'Labour Day',
-        '2012-06-04' => 'Foundation Day',
-        '2012-10-01' => 'Queens Birthday',
-        '2013-03-04' => 'Labour Day',
-        '2013-06-03' => 'Foundation Day',
-        '2013-09-30' => 'Queens Birthday'
-      },
-      'VIC' => {
-        '2011-03-14' => 'Labour Day',
-        '2011-04-25' => 'Easter Monday',
-        '2011-04-26' => 'ANZAC Day',
-        '2011-06-13' => 'Queens Birthday',
-        '2011-11-01' => 'Melbourne Cup Day',
-        '2012-03-12' => 'Labour Day',
-        '2012-06-11' => 'Queens Birthday',
-        '2012-11-06' => 'Melbourne Cup Day',
-        '2013-03-11' => 'Labour Day',
-        '2013-03-30' => 'Easter Saturday',
-        '2013-06-10' => 'Queens Birthday',
-        '2013-11-05' => 'Melbourne Cup Day'
-      },
-      'TAS' => {
-        '2011-02-14' => 'Royal Hobart Regatta',
-        '2011-03-14' => 'Eight Hours Day',
-        '2011-04-25' => 'ANZAC Day',
-        '2011-06-13' => 'Queens Birthday',
-        '2012-02-13' => 'Royal Hobart Regatta',
-        '2012-03-12' => 'Eight Hours Day',
-        '2012-06-11' => 'Queens Birthday',
-        '2013-03-11' => 'Eight Hours Day',
-        '2013-06-10' => 'Queens Birthday'
-      },
-      'NT' => {
-        '2011-04-25' => 'ANZAC Day',
-        '2011-05-02' => 'May Day',
-        '2011-06-13' => 'Queens Birthday',
-        '2011-08-01' => 'Picnic Day',
-        '2012-05-07' => 'May Day',
-        '2012-06-11' => 'Queens Birthday',
-        '2012-08-06' => 'Picnic Day',
-        '2013-03-30' => 'Easter Saturday',
-        '2013-06-10' => 'Queens Birthday'
-      },
-      'ACT' => {
-        '2011-03-14' => 'Canberra Day',
-        '2011-04-25' => 'Easter Monday',
-        '2011-04-26' => 'ANZAC Day',
-        '2011-06-13' => 'Queens Birthday',
-        '2011-10-03' => 'Labour Day',
-        '2012-03-12' => 'Canberra Day',
-        '2012-06-11' => 'Queens Birthday',
-        '2012-10-01' => 'Labour Day',
-        '2013-03-30' => 'Easter Saturday',
-        '2013-06-10' => 'Queens Birthday',
-        '2013-10-07' => 'Labour Day'
-      },
-      'National' => {
-        '2011-01-03' => 'New Years Day',
-        '2011-01-26' => 'Australia Day',
-        '2011-04-22' => 'Good Friday',
-        '2011-12-26' => 'Christmas Day',
-        '2011-12-27' => 'Boxing Day',
-        '2012-01-02' => 'New Years Day',
-        '2012-01-26' => 'Australia Day',
-        '2012-04-06' => 'Good Friday',
-        '2012-04-09' => 'Easter Monday',
-        '2012-04-25' => 'ANZAC Day',
-        '2012-12-25' => 'Christmas Day',
-        '2012-12-26' => 'Boxing Day',
-        '2013-01-01' => 'New Years Day',
-        '2013-01-28' => 'Australia Day',
-        '2013-03-29' => 'Good Friday',
-        '2013-03-31' => 'Easter Saturday',
-        '2013-04-01' => 'Easter Monday',
-        '2013-04-25' => 'ANZAC Day',
-        '2013-12-25' => 'Christmas Day',
-        '2013-12-26' => 'Boxing Day'
-      }
-    }
+    by(:state, :date)
+  end
+
+  def self.all
+    [
+      {:state => 'QLD',      :date => Date.new(2011,  4, 25), :title => 'ANZAC Day'              },
+      {:state => 'QLD',      :date => Date.new(2011,  4, 26), :title => 'Easter Monday'          },
+      {:state => 'QLD',      :date => Date.new(2011,  5,  2), :title => 'Labour Day'             },
+      {:state => 'QLD',      :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'QLD',      :date => Date.new(2011,  8, 17), :title => 'Royal Qland Show metro' },
+      {:state => 'QLD',      :date => Date.new(2012,  5,  7), :title => 'Labour Day'             },
+      {:state => 'QLD',      :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'QLD',      :date => Date.new(2012,  8, 15), :title => 'Royal Qland Show metro' },
+      {:state => 'QLD',      :date => Date.new(2013,  3, 30), :title => 'Easter Saturday'        },
+      {:state => 'QLD',      :date => Date.new(2013,  5,  6), :title => 'Labour Day'             },
+      {:state => 'QLD',      :date => Date.new(2013, 10,  7), :title => 'Queens Birthday'        },
+      {:state => 'SA',       :date => Date.new(2011,  3, 14), :title => 'Adelaide Cup'           },
+      {:state => 'SA',       :date => Date.new(2011,  4, 25), :title => 'Easter Monday'          },
+      {:state => 'SA',       :date => Date.new(2011,  4, 26), :title => 'ANZAC Day'              },
+      {:state => 'SA',       :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'SA',       :date => Date.new(2011, 10,  3), :title => 'Labour Day'             },
+      {:state => 'SA',       :date => Date.new(2012,  3, 12), :title => 'Adelaide Cup'           },
+      {:state => 'SA',       :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'SA',       :date => Date.new(2012, 10,  1), :title => 'Labour Day'             },
+      {:state => 'SA',       :date => Date.new(2013,  3, 11), :title => 'Adelaide Cup'           },
+      {:state => 'SA',       :date => Date.new(2013,  3, 30), :title => 'Easter Saturday'        },
+      {:state => 'SA',       :date => Date.new(2013,  6, 10), :title => 'Queens Birthday'        },
+      {:state => 'SA',       :date => Date.new(2013, 10,  7), :title => 'Labour Day'             },
+      {:state => 'NSW',      :date => Date.new(2011,  4, 25), :title => 'ANZAC Day'              },
+      {:state => 'NSW',      :date => Date.new(2011,  4, 26), :title => 'Easter Monday'          },
+      {:state => 'NSW',      :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'NSW',      :date => Date.new(2011, 10,  3), :title => 'Labour Day'             },
+      {:state => 'NSW',      :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'NSW',      :date => Date.new(2012, 10,  1), :title => 'Labour Day'             },
+      {:state => 'NSW',      :date => Date.new(2013,  3, 30), :title => 'Easter Saturday'        },
+      {:state => 'NSW',      :date => Date.new(2013,  6, 10), :title => 'Queens Birthday'        },
+      {:state => 'NSW',      :date => Date.new(2013, 10,  7), :title => 'Labour Day'             },
+      {:state => 'WA',       :date => Date.new(2011,  3,  7), :title => 'Labour Day'             },
+      {:state => 'WA',       :date => Date.new(2011,  4, 25), :title => 'Easter Monday'          },
+      {:state => 'WA',       :date => Date.new(2011,  4, 26), :title => 'ANZAC Day'              },
+      {:state => 'WA',       :date => Date.new(2011,  6,  6), :title => 'Foundation Day'         },
+      {:state => 'WA',       :date => Date.new(2011, 10,  3), :title => 'Queens Birthday'        },
+      {:state => 'WA',       :date => Date.new(2012,  3,  5), :title => 'Labour Day'             },
+      {:state => 'WA',       :date => Date.new(2012,  6,  4), :title => 'Foundation Day'         },
+      {:state => 'WA',       :date => Date.new(2012, 10,  1), :title => 'Queens Birthday'        },
+      {:state => 'WA',       :date => Date.new(2013,  3,  4), :title => 'Labour Day'             },
+      {:state => 'WA',       :date => Date.new(2013,  6,  3), :title => 'Foundation Day'         },
+      {:state => 'WA',       :date => Date.new(2013,  9, 30), :title => 'Queens Birthday'        },
+      {:state => 'VIC',      :date => Date.new(2011,  3, 14), :title => 'Labour Day'             },
+      {:state => 'VIC',      :date => Date.new(2011,  4, 25), :title => 'Easter Monday'          },
+      {:state => 'VIC',      :date => Date.new(2011,  4, 26), :title => 'ANZAC Day'              },
+      {:state => 'VIC',      :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'VIC',      :date => Date.new(2011, 11,  1), :title => 'Melbourne Cup Day'      },
+      {:state => 'VIC',      :date => Date.new(2012,  3, 12), :title => 'Labour Day'             },
+      {:state => 'VIC',      :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'VIC',      :date => Date.new(2012, 11,  6), :title => 'Melbourne Cup Day'      },
+      {:state => 'VIC',      :date => Date.new(2013,  3, 11), :title => 'Labour Day'             },
+      {:state => 'VIC',      :date => Date.new(2013,  3, 30), :title => 'Easter Saturday'        },
+      {:state => 'VIC',      :date => Date.new(2013,  6, 10), :title => 'Queens Birthday'        },
+      {:state => 'VIC',      :date => Date.new(2013, 11,  5), :title => 'Melbourne Cup Day'      },
+      {:state => 'TAS',      :date => Date.new(2011,  2, 14), :title => 'Royal Hobart Regatta'   },
+      {:state => 'TAS',      :date => Date.new(2011,  3, 14), :title => 'Eight Hours Day'        },
+      {:state => 'TAS',      :date => Date.new(2011,  4, 25), :title => 'ANZAC Day'              },
+      {:state => 'TAS',      :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'TAS',      :date => Date.new(2012,  2, 13), :title => 'Royal Hobart Regatta'   },
+      {:state => 'TAS',      :date => Date.new(2012,  3, 12), :title => 'Eight Hours Day'        },
+      {:state => 'TAS',      :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'TAS',      :date => Date.new(2013,  3, 11), :title => 'Eight Hours Day'        },
+      {:state => 'TAS',      :date => Date.new(2013,  6, 10), :title => 'Queens Birthday'        },
+      {:state => 'NT',       :date => Date.new(2011,  4, 25), :title => 'ANZAC Day'              },
+      {:state => 'NT',       :date => Date.new(2011,  5,  2), :title => 'May Day'                },
+      {:state => 'NT',       :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'NT',       :date => Date.new(2011,  8,  1), :title => 'Picnic Day'             },
+      {:state => 'NT',       :date => Date.new(2012,  5,  7), :title => 'May Day'                },
+      {:state => 'NT',       :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'NT',       :date => Date.new(2012,  8,  6), :title => 'Picnic Day'             },
+      {:state => 'NT',       :date => Date.new(2013,  3, 30), :title => 'Easter Saturday'        },
+      {:state => 'NT',       :date => Date.new(2013,  6, 10), :title => 'Queens Birthday'        },
+      {:state => 'ACT',      :date => Date.new(2011,  3, 14), :title => 'Canberra Day'           },
+      {:state => 'ACT',      :date => Date.new(2011,  4, 25), :title => 'Easter Monday'          },
+      {:state => 'ACT',      :date => Date.new(2011,  4, 26), :title => 'ANZAC Day'              },
+      {:state => 'ACT',      :date => Date.new(2011,  6, 13), :title => 'Queens Birthday'        },
+      {:state => 'ACT',      :date => Date.new(2011, 10,  3), :title => 'Labour Day'             },
+      {:state => 'ACT',      :date => Date.new(2012,  3, 12), :title => 'Canberra Day'           },
+      {:state => 'ACT',      :date => Date.new(2012,  6, 11), :title => 'Queens Birthday'        },
+      {:state => 'ACT',      :date => Date.new(2012, 10,  1), :title => 'Labour Day'             },
+      {:state => 'ACT',      :date => Date.new(2013,  3, 30), :title => 'Easter Saturday'        },
+      {:state => 'ACT',      :date => Date.new(2013,  6, 10), :title => 'Queens Birthday'        },
+      {:state => 'ACT',      :date => Date.new(2013, 10,  7), :title => 'Labour Day'             },
+      {:state => 'National', :date => Date.new(2011,  1,  3), :title => 'New Years Day'          },
+      {:state => 'National', :date => Date.new(2011,  1, 26), :title => 'Australia Day'          },
+      {:state => 'National', :date => Date.new(2011,  4, 22), :title => 'Good Friday'            },
+      {:state => 'National', :date => Date.new(2011, 12, 26), :title => 'Christmas Day'          },
+      {:state => 'National', :date => Date.new(2011, 12, 27), :title => 'Boxing Day'             },
+      {:state => 'National', :date => Date.new(2012,  1,  2), :title => 'New Years Day'          },
+      {:state => 'National', :date => Date.new(2012,  1, 26), :title => 'Australia Day'          },
+      {:state => 'National', :date => Date.new(2012,  4,  6), :title => 'Good Friday'            },
+      {:state => 'National', :date => Date.new(2012,  4,  9), :title => 'Easter Monday'          },
+      {:state => 'National', :date => Date.new(2012,  4, 25), :title => 'ANZAC Day'              },
+      {:state => 'National', :date => Date.new(2012, 12, 25), :title => 'Christmas Day'          },
+      {:state => 'National', :date => Date.new(2012, 12, 26), :title => 'Boxing Day'             },
+      {:state => 'National', :date => Date.new(2013,  1,  1), :title => 'New Years Day'          },
+      {:state => 'National', :date => Date.new(2013,  1, 28), :title => 'Australia Day'          },
+      {:state => 'National', :date => Date.new(2013,  3, 29), :title => 'Good Friday'            },
+      {:state => 'National', :date => Date.new(2013,  3, 31), :title => 'Easter Saturday'        },
+      {:state => 'National', :date => Date.new(2013,  4,  1), :title => 'Easter Monday'          },
+      {:state => 'National', :date => Date.new(2013,  4, 25), :title => 'ANZAC Day'              },
+      {:state => 'National', :date => Date.new(2013, 12, 25), :title => 'Christmas Day'          },
+      {:state => 'National', :date => Date.new(2013, 12, 26), :title => 'Boxing Day'             }
+    ]
   end
 end
--- test/test_all.rb
+++ test/test_all.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 require "#{File.dirname(__FILE__)}/../lib/oz_holidays"
 require 'date'
+require 'json'
 
 
 class TestOzHolidays < Test::Unit::TestCase
@@ -9,11 +10,21 @@ class TestOzHolidays < Test::Unit::TestCase
   end
   
   def test_anzac_day
-    assert_not_nil OzHolidays['NSW']["#{Date.today.year}-04-25"]
+    assert_not_nil OzHolidays['NSW'][Date.parse("#{Date.today.year}-04-25")]
+  end
+  
+  def test_all
+    assert_kind_of Array, OzHolidays.all
+    assert_kind_of Hash, OzHolidays.all.first
   end
   
   def test_to_json
     assert_not_nil OzHolidays.to_json
+  end
+  
+  def test_by_date
+    assert_kind_of Hash, OzHolidays.by_date
+    assert_kind_of Hash, OzHolidays.by_date.values.first
   end
   
   
@@ -21,6 +32,6 @@ private
   def find_nyd_hol
     nyd = Date.parse("#{Date.today.year}-01-01")
     nyd += 1 while(nyd.wday < 1 || nyd.wday > 5)
-    nyd.to_s
+    nyd
   end
 end
